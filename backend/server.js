58c073faf92c21ec60c4c5cff1956fd333dc464f
Soura Code <roymahapatrauttam@gmail.com>
2025-10-11 13:57:39 +0530

    cha

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/pixelgram')
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
